8432e226a3da46fa68d3837205dede1db77b7ec2
Bruno Fabre <bruno.hfabre@gmail.com>
2023-05-28 13:16:14 -0300

    first commit

---
+++ generate-token.js
@@ -0,0 +1,20 @@
+const googleServiceAccount = require('./google-service-account.json')
+const jwt = require('jsonwebtoken')
+
+function main() {
+  var token = jwt.sign(
+    { 
+      iss: googleServiceAccount.client_email,
+      scope: "https://www.googleapis.com/auth/androidmanagement",
+      aud: "https://oauth2.googleapis.com/token",
+      exp: Date.now() / 1000 + 60,
+      iat: Date.now() / 1000
+    }, 
+    googleServiceAccount.private_key, 
+    { algorithm: 'RS256' }
+  );
+
+  console.log({ token })
+}
+
+main()
